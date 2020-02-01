e08acb16774fcae836e83d0c435eebcf8e1abce4
League Student <league@iMac-5.attlocal.net>
2020-01-31 16:58:24 -0800

    on to module 4

---
+++ src/Check.java
@@ -0,0 +1,23 @@
+
+public class Check {
+	public static void main(String[] args) {
+
+		// for(int i = 0; i < 500; i++) {
+		// if(i % 2 == 1) {
+		// System.out.println(i);
+		// }
+		// }
+		for (int i = 1; i < 8; i++) {
+			if (i == 4) {
+				System.out.println("4,");
+			} else {
+				System.out.println(i + " potato,");
+
+			}
+			
+
+		}
+		System.out.println("more");
+	}
+	
+}
