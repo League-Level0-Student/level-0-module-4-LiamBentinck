ae854a8327d656c2e6534a8a14c789031cf1a798
LiamBentinck <50850141+LiamBentinck@users.noreply.github.com>
2020-03-23 11:25:06 -0700

    on virtual pet

--- src/_10_happy_pet/HappyPet.java
+++ src/_10_happy_pet/HappyPet.java
@@ -5,17 +5,19 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
+	static String pet;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		 pet = JOptionPane.showInputDialog("what pet do you want? Dog, Cat, Fish");
+		
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
 			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+					new String[] { "food", "water", "groom" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
 
@@ -27,4 +29,47 @@ public class HappyPet {
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	static void feed() {
+		happinessLevel+=1;
+		if (pet.equalsIgnoreCase("Dog")) {
+			JOptionPane.showMessageDialog(null, "dog barks");
+		}
+		else if(pet.equalsIgnoreCase("Cat")) {
+			JOptionPane.showMessageDialog(null, "cat purrs");
+		}
+		else if(pet.equalsIgnoreCase("Fish")) {
+			JOptionPane.showMessageDialog(null, "fish swims happily");
+		}
+	}
+	static void drink() {
+	happinessLevel+=1;
+	if (pet.equalsIgnoreCase("Dog")) {
+		JOptionPane.showMessageDialog(null, "dog wags tail and is now really happy");
+	}
+	if (pet.equalsIgnoreCase("Cat")) {
+		JOptionPane.showMessageDialog(null, "cat falls into bowl and starts to get angry at you");
+	}
+	if (pet.equalsIgnoreCase("Fish")) {
+		JOptionPane.showMessageDialog(null, "Fish is happy with new water");
+	}
+	}
+static void groom() {
+happinessLevel+1;
+if (pet.equalsIgnoreCase("Dog")) {
+	JOptionPane.showMessageDialog(null, "Dog wags tail");
+}
+if (pet.equalsIgnoreCase("Cat")) {
+	JOptionPane.showMessageDialog(null, "Cat acted like a lion and tried to eat you but just scratched your arm");
+}
+if (pet.equalsIgnoreCase("Fish")) {
+	JOptionPane.showMessageDialog(null, "Fish dies");
+}	
+	
+	
+	
+	
+	
+	
+	
+}
 }
